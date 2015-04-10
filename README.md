8ffbb9c381caccde9f4aeac2c0a923ccd5caede1
Ruslan Salikhov <ruslan.salihov@gmail.com>
2015-04-11 00:50:36 +0500

    Pre release

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This is a script for using self-compiled builds of the OpenSSL-library on the Android.
 
+Requires:
+makedepend (Ubuntu - xutils-dev)
+
 ```bash
 ./build-libssl.sh
 ```
--- build-libssl.sh
+++ build-libssl.sh
@@ -6,7 +6,10 @@
 #  for Android arm5
 #  Created by Ruslan Salikhov
 #
+#  References:
 #  https://wiki.openssl.org/index.php/Android
+#  https://github.com/x2on/OpenSSL-for-iPhone
+#  https://github.com/stdchpie/android-openssl (patching SONAME)
 #
 ###########################################################################
 #
@@ -27,32 +30,18 @@ pushd $BASEDIR
 BASEDIR=`pwd`
 
 if [ -z $ANDROID_NDK_ROOT ]; then
-    echo "Warning: ANDROID_NDK_ROOT is no set"
-    export ANDROID_NDK_ROOT=/jenkins/tools/android-ndk
+    echo "Error: ANDROID_NDK_ROOT is no set"
+    exit 1
 fi
 if [ ! -e $ANDROID_NDK_ROOT ]; then
     echo "Error: Cannot find Android NDK"
     exit 1
 fi
 
-. $BASEDIR/setenv.sh
-
-#export ANDROID_NDK_HOST=linux-x86_64
-#export ANDROID_NDK_PLATFORM=android-9
-#export ANDROID_TOOLCHAIN_NAME=arm-linux-androideabi
-#export ANDROID_TOOLCHAIN_VERSION=4.8
-#export ANDROID_NDK_TOOLCHAIN_VERSION=${ANDROID_TOOLCHAIN_VERSION}
-#export ANDROID_TOOLCHAIN=${ANDROID_TOOLCHAIN_NAME}-${ANDROID_TOOLCHAIN_VERSION}
-#export ANDROID_ARCH=armeabi
-#export ARCH=arm
-
-#export PATH=$ANDROID_NDK_ROOT:$PATH
-
-#export CC=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-$ANDROID_TOOLCHAIN_VERSION/prebuilt/$ANDROID_NDK_HOST/bin/arm-linux-androideabi-gcc
-#export AR=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-$ANDROID_TOOLCHAIN_VERSION/prebuilt/$ANDROID_NDK_HOST/bin/arm-linux-androideabi-ar
-#export ANDROID_DEV=$ANDROID_NDK_ROOT/platforms/android-9/arch-arm/usr
 
 set -e
+
+# Getting sources
 if [ ! -e openssl-${VERSION}.tar.gz ]; then
   echo "Downloading openssl-${VERSION}.tar.gz"
   curl https://www.openssl.org/source/openssl-${VERSION}.tar.gz -o openssl-${VERSION}.tar.gz
@@ -60,38 +49,56 @@ else
   echo "Using openssl-${VERSION}.tar.gz"
 fi
 
-rm -rf ${BASEDIR}/src
-rm -rf ${BASEDIR}/target
+# Getting setup android envitonment script
+if [ ! -e Setenv-android.sh ]; then
+  echo "Downloading Setenv-android.sh"
+  curl http://wiki.openssl.org/images/7/70/Setenv-android.sh -o Setenv-android.sh
+else
+  echo "Using Setenv-android.sh"
+fi
 
-mkdir -p "${BASEDIR}/src"
-mkdir -p "${BASEDIR}/target"
+ls
+chmod a+x Setenv-android.sh
+. ./Setenv-android.sh
 
-tar zxf openssl-${VERSION}.tar.gz -C "${BASEDIR}/src"
+# Removing possible temp folders
+rm -rf src
+rm -rf lib
+rm -rf include
+rm -rf install
+
+mkdir -p src
+
+tar zxf openssl-${VERSION}.tar.gz -C "./src"
 
 pushd "${BASEDIR}/src/openssl-${VERSION}"
 
 # Preparing sources
-patch -Np1 -i ${BASEDIR}/armv5-support.patch
 perl -pi -e 's/install: all install_docs install_sw/install: install_sw/g' Makefile.org
-export CC="gcc -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -finline-limit=64 -mfloat-abi=softfp"
 
 # Building
-./config shared no-ssl2 no-ssl3 no-comp no-hw no-engine --openssldir=$BASEDIR/target/
+./config shared no-ssl2 no-ssl3 no-comp no-hw no-engine --openssldir=$BASEDIR/install/
 
+# patch SONAME
+# took from https://github.com/stdchpie/android-openssl
+perl -pi -e 's/SHLIB_EXT=\.so\.\$\(SHLIB_MAJOR\)\.\$\(SHLIB_MINOR\)/SHLIB_EXT=\.so/g' Makefile
+perl -pi -e 's/SHARED_LIBS_LINK_EXTS=\.so\.\$\(SHLIB_MAJOR\) \.so//g' Makefile
+perl -pi -e 's/SHLIB_MAJOR=1/SHLIB_MAJOR=`/g' Makefile
+perl -pi -e 's/SHLIB_MINOR=0.0/SHLIB_MINOR=`/g' Makefile
+
+make clean
 make depend
 make all
 
-make install CC=$ANDROID_TOOLCHAIN/arm-linux-androideabi-gcc RANLIB=$ANDROID_TOOLCHAIN/arm-linux-androideabi-ranlib
+mkdir -p $BASEDIR/lib
+mkdir -p $BASEDIR/include
+cp -v libssl.{a,so} libcrypto.{a,so} $BASEDIR/lib
+cp -rvL include/openssl $BASEDIR/include
+
+#make install CC=$ANDROID_TOOLCHAIN/arm-linux-androideabi-gcc RANLIB=$ANDROID_TOOLCHAIN/arm-linux-androideabi-ranlib
 
 popd
 
-pushd target/lib
-rm -f libssl.so libcrypto.so
-rm -rf pkgconfig
-mv libssl.so.1.0.0 libssl.so
-mv libcrypto.so.1.0.0 libcrypto.so
-rpl -R -e libcrypto.so.1.0.0 "libcrypto.so\x00\x00\x00\x00\x00\x00" .
-rpl -R -e libssl.so.1.0.0 "libssl.so\x00\x00\x00\x00\x00\x00" .
 popd
 
-popd
+exit 0
